bcf099e807fe53ce2bd7a877cb976a314892e477
Str1xinka <105420059+Str1xinka@users.noreply.github.com>
2023-03-30 13:58:00 +0200

    update

--- src/Preview/preview.js
+++ src/Preview/preview.js
@@ -7,18 +7,24 @@ function Preview() {
     
    
     <><div><img class="flex left-48 bottom-391 absolute" src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div>
-      <div class="flex left-892 bottom-541 absolute">
+      <div class="flex left-892 bottom-605 absolute">
+       <ul>
+         <li class="bg-black text-gray-500 p-6">Preview</li>
+         <li class="bg-black text-gray-500 p-6">Combo</li>
+         <li class="bg-black text-gray-500 p-6 w-28 text-xs">PGM Direct</li>
+       </ul>
+     </div>
+     <div class="flex left-990 bottom-605 absolute">
        <ul>
          <li class="bg-black text-gray-500 p-6">Layer 1</li>
          <li class="bg-black text-gray-500 p-6">Layer 2</li>
          <li class="bg-black text-gray-500 p-6 w-28">Layer 5</li>
        </ul>
      </div>
-     <div class="flex left-990 bottom-541 absolute">
+     <div class="flex left-892 bottom-540 absolute">
        <ul>
-         <li class="bg-black text-gray-500 p-6">Layer 1</li>
-         <li class="bg-black text-gray-500 p-6">Layer 2</li>
-         <li class="bg-black text-gray-500 p-6 w-28">Layer 5</li>
+         <li class="bg-black text-gray-500 p-6 w-50 text-center">Layer 1</li>
+         
        </ul>
      </div>
     <div class="left-1093 flex bottom-391 absolute"><img  src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div></>
