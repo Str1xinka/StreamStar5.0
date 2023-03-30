f6163d0876fe5e3c7decace798b2c07051c3a587
Str1xinka <105420059+Str1xinka@users.noreply.github.com>
2023-03-30 13:34:18 +0200

    update

--- src/Preview/preview.js
+++ src/Preview/preview.js
@@ -7,22 +7,18 @@ function Preview() {
     
    
     <><div><img class="flex left-48 bottom-391 absolute" src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div>
-      <div class="flex left-892 bottom-501 absolute">
+      <div class="flex left-892 bottom-541 absolute">
        <ul>
-         <li class="bg-black text-gray-500 ">Layer 1</li>
-         <li class="bg-black text-gray-500 ">Layer 2</li>
-         <li class="bg-black text-gray-500 ">Layer 3</li>
-         <li class="bg-black text-gray-500 ">Layer 4</li>
-         <li class="bg-black text-gray-500 w-24">Layer 5</li>
+         <li class="bg-black text-gray-500 p-6">Layer 1</li>
+         <li class="bg-black text-gray-500 p-6">Layer 2</li>
+         <li class="bg-black text-gray-500 p-6 w-28">Layer 5</li>
        </ul>
      </div>
-     <div class="flex left-892 bottom-501 absolute">
+     <div class="flex left-990 bottom-541 absolute">
        <ul>
-         <li class="bg-black text-gray-500 ">Layer 1</li>
-         <li class="bg-black text-gray-500 ">Layer 2</li>
-         <li class="bg-black text-gray-500 ">Layer 3</li>
-         <li class="bg-black text-gray-500 ">Layer 4</li>
-         <li class="bg-black text-gray-500 w-24">Layer 5</li>
+         <li class="bg-black text-gray-500 p-6">Layer 1</li>
+         <li class="bg-black text-gray-500 p-6">Layer 2</li>
+         <li class="bg-black text-gray-500 p-6 w-28">Layer 5</li>
        </ul>
      </div>
     <div class="left-1093 flex bottom-391 absolute"><img  src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div></>
