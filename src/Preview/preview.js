70a27f941318328a5ef58790368a943b11a1a87d
Str1xinka <105420059+Str1xinka@users.noreply.github.com>
2023-03-30 20:04:22 +0200

    bum

--- src/Preview/preview.js
+++ src/Preview/preview.js
@@ -3,10 +3,11 @@ import stream1 from './stream1.png';
 
 function Preview() {
   return (
-           
-    
-   
-    <><div><img class="flex left-48 bottom-391 absolute" src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div>
+
+
+    <div> 
+    <div>
+      <img class="flex  left-48 bottom-391 absolute" src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div>
       <div class="flex left-892 bottom-605 absolute">
        <ul>
          <li class="bg-black text-gray-500 p-6">Preview</li>
@@ -17,29 +18,31 @@ function Preview() {
      <div class="flex left-990 bottom-605 absolute">
        <ul>
          <li class="bg-black text-gray-500 p-6">Layer 1</li>
-         <li class="bg-black text-gray-500 p-6">Layer</li>
+         <li class="bg-black text-gray-500 p-6">Layer 2</li>
          <li class="bg-black text-gray-500 p-6 w-28">Layer 5</li>
        </ul>
      </div>
      <div class="flex left-892 bottom-540 absolute">
        <ul>
          <li class="bg-black text-gray-500 p-6 w-50 text-center">Layer 1</li>
-         
+
        </ul>
      </div>
-    <div class="left-1093 flex bottom-391 absolute"><img  src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div></>
-    
-    
+    <div class="left-1093 flex bottom-391 absolute"><img  src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img>
+    </div>
+    </div>
 
-    
-    
-      
-       
-        
-    
-    
-       
-     
+
+
+
+
+
+
+
+
+
+
+
   );
 }
 
