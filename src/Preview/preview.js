9e9f9d65f2f214c6f5065d95395634c280a0f53a
Str1xinka <105420059+Str1xinka@users.noreply.github.com>
2023-04-02 03:46:43 +0200

    j

--- src/Preview/preview.js
+++ src/Preview/preview.js
@@ -7,6 +7,7 @@ function Preview() {
   return (
            
     
+<<<<<<< HEAD
       <div class=" flex bottom-892 left-48 relative overflow-hidden">   
      <div class=" border-b-6 border-b-green-500"><img class="   " src={stream1} alt="" style={{ width: '700px', height: '415px' }}></img></div>
       <div class="h-16">
@@ -15,6 +16,27 @@ function Preview() {
          <li class="bg-black text-gray-500 pl-8 h-16 relative m-1"> <img class=" flex relative  left-1" src={playbutton3} alt=""  style={{width: '50px', height:'30px'}}></img>COMBO  </li>
          <li class="bg-black text-gray-500 pl-8 h-16 relative  m-1 "><img class="flex relative left-2" src={playbutton2} alt="" style={{width: '40px', height:'25px'}}></img> PGM DEFECT</li>
 
+=======
+   
+    <><div><img class="flex left-48 bottom-391 absolute" src={stream1} alt="" style={{ width: '700px', height: '430px' }}></img></div>
+      <div class="flex left-892 bottom-605 absolute">
+       <ul>
+         <li class="bg-black text-gray-500 p-6">Preview</li>
+         <li class="bg-black text-gray-500 p-6">Combo</li>
+         <li class="bg-black text-gray-500 p-6 w-28 text-xs">PGM Direct</li>
+       </ul>
+     </div>
+     <div class="flex left-990 bottom-605 absolute">
+       <ul>
+         <li class="bg-black text-gray-500 p-6">Layer 1</li>
+         <li class="bg-black text-gray-500 p-6">Layer 2</li>
+         <li class="bg-black text-gray-500 p-6 w-28">Layer 5</li>
+       </ul>
+     </div>
+     <div class="flex left-892 bottom-540 absolute">
+       <ul>
+         <li class="bg-black text-gray-500 p-6 w-50 text-center">Layer 1</li>
+>>>>>>> parent of 4acd817 (d)
          
        </ul>
        
